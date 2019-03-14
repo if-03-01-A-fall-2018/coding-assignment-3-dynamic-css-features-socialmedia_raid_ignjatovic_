ac10685b67b879cd0c60cd7295badc05f2733474
davidenkovic <davidenkovic100@gmail.com>
2019-03-14 23:49:21 +0100

    level up again

--- JS/poll.js
+++ JS/poll.js
@@ -27,3 +27,61 @@ function question1(){
     number = number + 5;
   }
 }
+function question2(){
+  var checkBoxForSubmit = document.getElementById("submit");
+  var checkBoxForAnswer1 = document.getElementById("a1");
+  var checkBoxForAnswer2 = document.getElementById("a2");
+  var checkBoxForAnswer3 = document.getElementById("a3");
+  var checkBoxForAnswer4 = document.getElementById("a4");
+  var checkBoxForAnswer5 = document.getElementById("a5");
+
+  var number = 0;
+
+  if (checkBoxForSubmit.checked == true){
+    text.style.display = "block";
+  }
+  else if (checkBoxForAnswer1.checked == false) {
+    number = number + 1;
+  }
+  else if (checkBoxForAnswer2.checked == false) {
+    number = number + 2;
+  }
+  else if (checkBoxForAnswer3.checked == false) {
+    number = number + 3;
+  }
+  else if (checkBoxForAnswer4.checked == false) {
+    number = number + 4;
+  }
+  else if (checkBoxForAnswer5.checked == false) {
+    number = number + 5;
+  }
+}
+function question3(){
+  var checkBoxForSubmit = document.getElementById("submit");
+  var checkBoxForAnswer1 = document.getElementById("a1");
+  var checkBoxForAnswer2 = document.getElementById("a2");
+  var checkBoxForAnswer3 = document.getElementById("a3");
+  var checkBoxForAnswer4 = document.getElementById("a4");
+  var checkBoxForAnswer5 = document.getElementById("a5");
+
+  var number = 0;
+
+  if (checkBoxForSubmit.checked == true){
+    text.style.display = "block";
+  }
+  else if (checkBoxForAnswer1.checked == false) {
+    number = number + 1;
+  }
+  else if (checkBoxForAnswer2.checked == false) {
+    number = number + 2;
+  }
+  else if (checkBoxForAnswer3.checked == false) {
+    number = number + 3;
+  }
+  else if (checkBoxForAnswer4.checked == false) {
+    number = number + 4;
+  }
+  else if (checkBoxForAnswer5.checked == false) {
+    number = number + 5;
+  }
+}
